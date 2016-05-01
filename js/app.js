682ba8ca8e3e7f4e674ae6ed7b0d6eba3df14e6f
sawheeldon <simon@jampot.design>
2016-05-01 23:51:10 +0100

    Background and fix hadouken

--- js/app.js
+++ js/app.js
@@ -20,24 +20,40 @@ $(document).ready(function () {
         // play hadouken sound
         $('.hadouken').finish().show()
         .animate(
-            {'left': '1020px'},
+            {'left': '300px'},
             500,
             function () {
                 $(this).hide();
-                $(this).css('left','520px');
+                $(this).css('left','-212px');
         }
     );
     })
     /* mouse up */
     $('.ryu').mouseup(function () {
         $('.ryu-action').hide();
-        $('.ryu-ready').show()
+        $('.ryu-still').show();
         // ryu goes back to his ready position
-    })
-});
+    });
+    /* keydown */
+    $(document).keydown(function (key) {
+        if (key.keyCode == 88) {
+            $('.ryu-action').hide();
+            $('.ryu-cool').show();
+        }
+    });
 
-function playHadouken () {
-    $('#hadouken-sound') [0].volume = 0.5;
-    $('#hadouken-sound') [0].load();
-    $('#hadouken-sound') [0].play();
-}
+    /* key up */
+    $(document).keyup(function (key) {
+        if (key.keyCode == 88) {
+            $('ryu-action').hide();
+            $('ryu-still').show();
+        }
+    });
+});
+    /* Sounds */
+
+    function playHadouken () {
+        $('#hadouken-sound') [0].volume = 0.5;
+        $('#hadouken-sound') [0].load();
+        $('#hadouken-sound') [0].play();
+    }
