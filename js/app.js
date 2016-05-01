16d8810e749d53990e290f4cb763591bffbe19f0
sawheeldon <simon@jampot.design>
2016-05-01 11:58:04 +0100

    Hadouken

--- js/app.js
+++ js/app.js
@@ -5,10 +5,31 @@ $(document).ready(function () {
     $('.ryu').mouseenter(function () {
         $('.ryu-action').hide();
         $('.ryu-ready').show();
-    });
+    })
     /* mouse leaves*/
     $('.ryu').mouseleave(function () {
         $('.ryu-action').hide();
         $('.ryu-still').show();
-    });
+    })
+    /* mouse down */
+    $('.ryu').mousedown(function () {
+        $('.ryu-action').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').show();
+        // play hadouken sound
+        $('.hadouken').show().animate(
+        {'left': '1020px'},
+        500,
+        function () {
+            $(this).hide();
+            $(this).css('left','520px');
+        }
+    );
+    })
+    /* mouse up */
+    $('.ryu').mouseup(function () {
+        $('.ryu-action').hide();
+        $('.ryu-ready').show()
+        // ryu goes back to his ready position
+    })
 });
